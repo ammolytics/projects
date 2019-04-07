792d0a1ecf462c9477ca64037e87303bf5929a31
Eric Higgins <erichiggins@gmail.com>
2019-04-07 08:33:13 -0700

    Only install bluetooth-hci-socket for linux

--- trickler/peripheral/bin/setup.sh
+++ trickler/peripheral/bin/setup.sh
@@ -4,9 +4,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   # OSX
   echo "OSX";
   xcode-select --install;
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux
-  echo "Linux";
 fi
 
 nodeenv -n 10.15.3 .venv
@@ -15,4 +12,9 @@ npm install -g npm
 npm install -g prebuild
 npm install -g node-gyp
 npm install
-npm install bluetooth-hci-socket@npm:@abandonware/bluetooth-hci-socket
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  echo "Linux";
+  npm install bluetooth-hci-socket@npm:@abandonware/bluetooth-hci-socket;
+fi
