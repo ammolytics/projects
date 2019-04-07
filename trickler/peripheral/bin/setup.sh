594e43b31cc0fc49647ae6bacfb09fab62494502
Eric Higgins <erichiggins@gmail.com>
2019-04-07 10:19:55 -0700

    Add more setup steps for osx/linux

--- trickler/peripheral/bin/setup.sh
+++ trickler/peripheral/bin/setup.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # OSX
-  echo "OSX";
+  echo "OSX pre-install";
   xcode-select --install;
+  nodeenv -n 8.15.1 .venv
+  source .venv/bin/activate
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux
+  echo "Linux pre-install";
+  if ! [ -x "$(command -v node)" ]; then
+    echo "Installing Node."
+    wget https://nodejs.org/dist/latest-v8.x/node-v8.15.1-linux-armv7l.tar.xz;
+    sudo mkdir -p /usr/local/lib/nodejs;
+    sudo tar -xJvf node-v8.15.1-linux-armv7l.tar.xz -C /usr/local/lib/nodejs;
+    rm node-v8.15.1-linux-armv7l.tar.xz;
+    echo "\nexport PATH=$PATH:/usr/local/lib/nodejs/node-v8.15.1-linux-armv7l/bin" >> ~/.profile;
+    source ~/.profile;
+  fi
 fi
 
-nodeenv -n 8.15.1 .venv
-source .venv/bin/activate
+
 npm install -g npm
 npm install -g prebuild
 npm install -g node-gyp
 npm install
+
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
