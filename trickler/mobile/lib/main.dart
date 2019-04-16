63862a554a8416d749acccdf01a378cad4304628
Macuyler <macuyler@gmail.com>
2019-04-16 18:22:02 -0500

    Moved bluetooth logic to bluetooth.dart

--- trickler/mobile/lib/main.dart
+++ trickler/mobile/lib/main.dart
@@ -7,10 +7,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:redux/redux.dart';
-import 'dart:async';
 
-import 'globals.dart';
-import 'actions.dart';
+import 'bluetooth.dart';
 import 'models/index.dart';
 import 'reducers/index.dart';
 import 'pages/home.dart';
@@ -23,77 +21,10 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends BluetoothApp {
   final Store<AppState> store;
   MyApp({ Key key, this.store }) : super(key: key);
   final FlutterBlue flutterBlue = FlutterBlue.instance;
-
-  _connectToDevice(BluetoothDevice device) {
-    dynamic deviceConnection = flutterBlue
-      .connect(device, timeout: Duration(seconds: 4))
-      .listen((s) {
-        store.dispatch(SetConnectionStatus(s));
-        if (s == BluetoothDeviceState.connected) {
-          _handleDeviceStateChange();
-        } else {
-          _disconnect();
-        }
-      }, onDone: _disconnect);
-    store.dispatch(SetDeviceConnection(deviceConnection));
-  }
-
-  _handleDeviceStateChange() {
-    store.state.deviceState.device.discoverServices().then((services) {
-      List<BluetoothCharacteristic> chars = [];
-      services.forEach((service) {
-        if (service.uuid.toString() == TRICKLER_SERVICE_UUID) {
-          store.dispatch(SetService(service));
-          chars = service.characteristics;
-        }
-      });
-      _readCharacteristics(chars, 0);
-    });
-  }
-  
-  _readCharacteristics(List<BluetoothCharacteristic> chars, int i) {
-    BluetoothDevice device = store.state.deviceState.device;
-    // Asynchronously read & subscribe to characteristics one at a time
-
-    BluetoothCharacteristic char = chars[i];
-    if (char.properties.read && DONT_READ_CHARS.indexOf(char.uuid.toString()) == -1) {
-      device.readCharacteristic(char).then((readChar) async {
-        store.dispatch(SetCharacteristic(char.uuid, readChar));
-        if (char.properties.notify) {
-          // Subscribe to chars with notifications
-          await device.setNotifyValue(char, true);
-          StreamSubscription sub = device.onValueChanged(char).listen((data) {
-            store.dispatch(SetCharacteristic(char.uuid, data));
-          });
-          store.dispatch(AddSubscription(sub));
-        }
-
-        if (i + 1 < chars.length) {
-          _readCharacteristics(chars, i + 1);
-        }
-      });
-    } else if (DONT_READ_CHARS.indexOf(char.uuid.toString()) == -1) {
-      print('\n\n\nCAN\'T READ ${char.uuid.toString()}');
-      print('\n\n${char.uuid.toString()} PROPERTIES');
-      print('NOTIFY: ${char.properties.notify}');
-      print('READ: ${char.properties.read}');
-      print('WRITE: ${char.properties.write}\n\n');
-    }
-  }
-
-  _disconnect() {
-    store.state.deviceState.characteristics.subscriptions.forEach((sub) {
-      sub.cancel();
-    });
-    store.dispatch(ClearSubscriptions());
-    print('\n\n\nDisconnecting...\n\n\n\n');
-    store.state.deviceState.deviceConnection?.cancel();
-    store.dispatch(ResetDeviceState());
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -109,8 +40,8 @@ class MyApp extends StatelessWidget {
         ),
         home: HomePage(
           key: Key('HomePage'),
-          connectToDevice: (device) => _connectToDevice(device),
-          disconnect: () => _disconnect(),
+          connectToDevice: (device) => connectToDevice(device),
+          disconnect: () => disconnect(),
         ),
       ),
     );
