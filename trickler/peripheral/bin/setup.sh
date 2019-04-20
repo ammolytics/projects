12d2290f4f7c715f5dd8b3b508dd303321ab00c1
Eric Higgins <erichiggins@gmail.com>
2019-04-20 14:45:57 -0700

    Fix node install for Pi Zero W

--- trickler/peripheral/bin/setup.sh
+++ trickler/peripheral/bin/setup.sh
@@ -5,20 +5,20 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   # OSX
   echo "OSX pre-install";
   xcode-select --install;
-  nodeenv -n 8.15.1 .venv
+  nodeenv -n 8.16.0 .venv
   source .venv/bin/activate
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
   echo "Linux pre-install";
   if ! [ -x "$(command -v node)" ]; then
     echo "Installing Node."
-    wget https://nodejs.org/dist/latest-v8.x/node-v8.15.1-linux-armv7l.tar.xz;
+    wget https://nodejs.org/dist/v8.16.0/node-v8.16.0-linux-armv6l.tar.xz;
     sudo mkdir -p /usr/local/lib/nodejs;
-    sudo tar -xJvf node-v8.15.1-linux-armv7l.tar.xz -C /usr/local/lib/nodejs;
-    rm node-v8.15.1-linux-armv7l.tar.xz;
-    sudo ln -s /usr/local/lib/nodejs/node-v8.15.1-linux-armv7l/bin/node /usr/bin/node
-    sudo ln -s /usr/local/lib/nodejs/node-v8.15.1-linux-armv7l/bin/npm /usr/bin/npm
-    sudo ln -s /usr/local/lib/nodejs/node-v8.15.1-linux-armv7l/bin/npx /usr/bin/npx
+    sudo tar -xJvf node-v8.16.0-linux-armv6l.tar.xz -C /usr/local/lib/nodejs;
+    rm node-v8.16.0-linux-armv6l.tar.xz;
+    sudo ln -fs /usr/local/lib/nodejs/node-v8.16.0-linux-armv6l/bin/node /usr/bin/node
+    sudo ln -fs /usr/local/lib/nodejs/node-v8.16.0-linux-armv6l/bin/npm /usr/bin/npm
+    sudo ln -fs /usr/local/lib/nodejs/node-v8.16.0-linux-armv6l/bin/npx /usr/bin/npx
   fi
 fi
 
