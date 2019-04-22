f8b81f58a8191451d4a8d4aceb143969ff36240d
Macuyler <macuyler@gmail.com>
2019-04-22 11:05:31 -0500

    Added github links to about.dart

--- trickler/mobile/lib/pages/about.dart
+++ trickler/mobile/lib/pages/about.dart
@@ -2,7 +2,11 @@
 /// Released under the MIT license. See LICENSE file in the project root for details.
 
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../widgets/header.dart';
+
+/// AboutPage is a StatelessWidget that provides the user a description
+/// of the project, as well as links to the repo and license.
 
 class AboutPage extends StatelessWidget {
   AboutPage({ Key key }) : super(key: key);
@@ -13,6 +17,33 @@ This is the Open Trickler Mobile App. This app allows you to connect to your Ope
 
 Open Trickler is an open source project by Ammolytics.
   """;
+
+  /// _launchUrl attempts to open the given url in the users default
+  /// browser. If it is unable to open the url it will thow an error.
+
+  void _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  /// _getLink returns a Flat button that displays the
+  /// given text, and opens the given url onPressed.
+
+  Widget _getLink(String text, String url) => FlatButton(
+    onPressed: () => _launchUrl(url),
+    child: Text(
+      text,
+      style: TextStyle(
+        fontSize: 15,
+        color: Colors.blue,
+      ),
+    ),
+  );
+
+  /// _getHeading returns a SizedBox that contains the logo, and title of the page.
 
   Widget _getHeading() => SizedBox(
     height: 60,
@@ -47,16 +78,29 @@ Open Trickler is an open source project by Ammolytics.
           children: <Widget>[
             _getHeading(),
             Padding(
-              padding: EdgeInsets.only(bottom: 25),
+              padding: EdgeInsets.only(bottom: 35),
               child: Text(
                 '\u00a9 2019 Ammolytics',
                 style: TextStyle(
-                  fontSize: 15,
+                  fontSize: 13,
                   color: Color.fromARGB(255, 115, 115, 115),
                 ),
               ),
             ),
-            Text(this.about)
+            Text(
+              this.about,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                _getLink('GITHUB', 'https://github.com/ammolytics/projects'),
+                _getLink('LICENSE', 'https://github.com/ammolytics/projects/blob/master/LICENSE'),
+              ],
+            ),
           ],
         ),
       ),
