43963fa2618cb4689f33a52faf8835e2e0e1c2bc
Eric Higgins <erichiggins@gmail.com>
2019-05-05 00:29:51 -0700

    Update last two characteristics to newer class def

--- trickler/peripheral/serial-number-characteristic.js
+++ trickler/peripheral/serial-number-characteristic.js
@@ -2,46 +2,45 @@
  * Copyright (c) Ammolytics and contributors. All rights reserved.
  * Released under the MIT license. See LICENSE file in the project root for details.
  */
-const util = require('util')
 const bleno = require('bleno')
 
 /**
 "nknown command: "SN,15641060
 */
 
-function SerialNumberCharacteristic(trickler) {
-  bleno.Characteristic.call(this, {
-    uuid: '2a25',
-    properties: ['read'],
-    descriptors: [
-      new bleno.Descriptor({
-        uuid: '2901',
-        value: 'Scale serial number'
-      })
-    ]
-  })
+class SerialNumberCharacteristic extends bleno.Characteristic {
 
-  this.trickler = trickler
-}
+  constructor (trickler) {
+    super({
+      uuid: '2a25',
+      properties: ['read'],
+      descriptors: [
+        new bleno.Descriptor({
+          uuid: '2901',
+          value: 'Scale serial number'
+        })
+      ]
+    })
+
+    this.trickler = trickler
+  }
 
 
-util.inherits(SerialNumberCharacteristic, bleno.Characteristic)
-
-
-SerialNumberCharacteristic.prototype.onReadRequest = function(offset, callback) {
-  console.log(`serial number read request`)
-  if (offset) {
-    callback(this.RESULT_ATTR_NOT_LONG, null)
-  } else {
-    if (typeof this.trickler.serialNumber === 'undefined') {
-      this.trickler.once('serialNumber', serialNumber => {
-        var data = Buffer.from(serialNumber)
-        callback(this.RESULT_SUCCESS, data)
-      })
-      this.trickler.getSerialNumber()
+  onReadRequest (offset, callback) {
+    console.log(`serial number read request`)
+    if (offset) {
+      callback(this.RESULT_ATTR_NOT_LONG, null)
     } else {
-      var data = Buffer.from(this.trickler.serialNumber)
-      callback(this.RESULT_SUCCESS, data)
+      if (typeof this.trickler.serialNumber === 'undefined') {
+        this.trickler.once('serialNumber', serialNumber => {
+          var data = Buffer.from(serialNumber)
+          callback(this.RESULT_SUCCESS, data)
+        })
+        this.trickler.getSerialNumber()
+      } else {
+        var data = Buffer.from(this.trickler.serialNumber)
+        callback(this.RESULT_SUCCESS, data)
+      }
     }
   }
 }
