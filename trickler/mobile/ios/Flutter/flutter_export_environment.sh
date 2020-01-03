cd647e46b0c137ecee3d5bbc8baa92d7cbc7b204
Eric Higgins <erichiggins@gmail.com>
2020-01-03 13:18:01 -0800

    Updated app icon. Added push notification capability to resolve error. Defined target property values. Change app dev to Eric

--- trickler/mobile/ios/Flutter/flutter_export_environment.sh
+++ trickler/mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/macuylerdunn/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/macuylerdunn/Code/projects/LEGACY/Ammolytics/projects/trickler/mobile"
-export "FLUTTER_TARGET=/Users/macuylerdunn/Code/projects/LEGACY/Ammolytics/projects/trickler/mobile/lib/main.dart"
+export "FLUTTER_ROOT=/Users/ronin/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ronin/src/ammolytics/projects/trickler/mobile"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/macuylerdunn/Library/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ronin/src/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
