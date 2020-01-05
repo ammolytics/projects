22bb422fc7ea1b8fec9e1a0d413a009def958edf
Macuyler <macuyler@gmail.com>
2020-01-04 18:17:42 -0600

    Remove boilerplate comments and code

--- trickler/mobile/test/widget_test.dart
+++ trickler/mobile/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile/main.dart';
+import 'package:opentrickler/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
