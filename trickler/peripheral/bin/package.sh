f9ea77c4689369e1c4f98263d44fcc45b774f7db
Eric Higgins <erichiggins@gmail.com>
2020-05-28 00:32:28 -0700

    Updates and fixes. Details in PR.

---
+++ trickler/peripheral/bin/package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sed_expr="s,^,opentrickler/,"
+readonly version=$(head -n 1 VERSION)
+out=opentrickler-${version}.tar
+
+tar --create --transform "$sed_expr" --file $out \
+  LICENSE \
+  VERSION \
+  README.md \
+  ecosystem.config.js \
+  index.js \
+  package.json \
+  server.js \
+  server.sh \
+  lib/
+
+xz -c $out > $out.xz
