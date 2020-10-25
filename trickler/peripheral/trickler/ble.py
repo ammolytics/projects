f3d72bf67adf4ee142ab4031d6afd2530931d3a1
Eric Higgins <erichiggins@gmail.com>
2020-10-25 16:00:35 -0700

    First commit of python files -- not working

---
+++ trickler/peripheral/trickler/ble.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import pybleno
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Test bluetooth')
+    args = parser.parse_args()
+
+    main(args)
