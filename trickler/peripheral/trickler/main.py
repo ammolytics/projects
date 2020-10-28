0083f7aeb0558e80eb69a44a08a38cda32e6e338
Eric Higgins <erichiggins@gmail.com>
2020-10-27 22:51:22 -0700

    Code cleanups

--- trickler/peripheral/trickler/main.py
+++ trickler/peripheral/trickler/main.py
@@ -1,47 +1,49 @@
 import serial
 
 import gpiozero
+import pymemcache.client.base
 
+import scales
 import PID
+
+
+memcache = pymemcache.client.base.Client('localhost')
 
 
 def trickler_loop(pid, pwm, scale, args):
     running = True
+    memcache.set('running', running)
 
     while running:
         # Read scale values (weight/unit/stable)
         # Read settings (on/off/target/etc)
 
-        config = getConfig()
-        running = config.running
+        scale.update()
 
         pid.update(scale.weight)
-        targetPwm = pid.output
-        targetPwm = max(min(int(targetPwm), args.maxPwm), args.minPwm)
+        target_pwm = pid.output
+        target_pwm = max(min(int(target_pwm), args.max_pwm), args.min_pwm)
 
-        pwm.value = targetPwm / 100
+        pwm.value = target_pwm / 100
+
+        target_weight = memcache.get('target_weight')
 
         total_divs = target_weight / unit_res
-        remainder_weight = target_weight - current_weight
+        remainder_weight = target_weight - scale.weight
         remainder_divs = remainder_weight / unit_res
         remainder_perc = (remainder_divs / total_divs) * 100
 
-
-def getConfig():
-    pass
+        running = memcache.get('running', running)
 
 
 def main(args):
-    P = 10
-    I = 1
-    D = 1
-    pid = PID.PID(P, I, D)
-    pwm = gpiozero.PWMOutputDevice(args.motorPIN)
+    Proportional = 10
+    Integral = 1
+    Derivative = 1
+    pid = PID.PID(Proportional, Integral, Derivative)
+    pwm = gpiozero.PWMOutputDevice(args.motor_pin)
 
-    scale = serial.Serial()
-    scale.port = args.devPath
-    scale.baudrate = args.baudRate
-    scale.timeout = args.scaleTimeout
+    scale = scales.SCALES[args.scale](port=args.scale_port, baudrate=args.scale_baudrate, timeout=args.scale_timeout)
 
     trickler_loop(pid, pwm, scale, args)
 
@@ -51,5 +53,12 @@ if __name_ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run OpenTrickler.')
     args = parser.parse_args()
+    # TODO(eric): args.scale
+    # TODO(eric): args.scale_port
+    # TODO(eric): args.scale_baudrate
+    # TODO(eric): args.scale_timeout
+    # TODO(eric): args.motor_pin
+    # TODO(eric): args.max_pwm
+    # TODO(eric): args.min_pwm
 
     main(args)
