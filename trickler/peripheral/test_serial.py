c29bf227117b1bd8e6b310f776964254f1ca2e16
Eric Higgins <erichiggins@gmail.com>
2020-10-30 23:49:25 -0700

    Drop test serial file, replaced my scales.py

--- trickler/peripheral/test_serial.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import serial
-import datetime
-
-
-scale = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, timeout=0.05)
-
-while 1:
-    print(datetime.datetime.now().isoformat(), scale.readline())
