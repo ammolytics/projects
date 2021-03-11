3be3746ba99441a0575e82983bb1b79f2eff965d
Eric Higgins <erichiggins@gmail.com>
2021-03-11 00:22:36 -0800

    Rename config.ini to opentrickler_config.ini to avoid confusion with config.txt on the BOOT partition.

--- trickler/peripheral/server.sh
+++ trickler/peripheral/server.sh
@@ -2,4 +2,4 @@
 
 # Enter code directory and start the trickler daemon.
 cd /code/opentrickler
-exec ./trickler/main.py config.ini >> "$1" 2>&1
+exec ./trickler/main.py opentrickler_config.ini >> "$1" 2>&1
